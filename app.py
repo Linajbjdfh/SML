4c284b686a8489a499c67de24953a75bf85a1c9f
Lina Tarasenko <113516299+Linajbjdfh@users.noreply.github.com>
2022-10-05 14:14:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,6 +78,6 @@ if st.button('Predict! 🚀'):
     predicted_value = model_xgb.predict(line_to_pred)[0]
 
     #print out result to user
-    st.metric(label="Predicted Attrition", value=f'{predicted_value}')
+    st.metric(label="Predicted Attrition", value=f'round({predicted_value})')
     
    
