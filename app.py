ea9bd633482e15d0fa1499d06fc6c6da56c3e656
Lina Tarasenko <113516299+Linajbjdfh@users.noreply.github.com>
2022-10-05 14:02:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def read_objects():
     return model_xgb, scaler, ohe, cats, shap_values
 
 model_xgb, scaler, ohe, cats, shap_values = read_objects()
-st.write(ohe)
 
 with st.expander("What's that app?"):
     st.markdown("""
